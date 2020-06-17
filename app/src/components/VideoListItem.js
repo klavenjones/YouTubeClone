baf1ed4e8a4b3c091a954ae76778bb87d1f6041e
Klaven Jones <klavensjones@gmail.com>
2020-06-17 00:43:22 -0400

    Added Search Bar

--- app/src/components/VideoListItem.js
+++ app/src/components/VideoListItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export const VideoListItem = ({ video, onVideoSelect }) => {
-  const { url, height, width } = video.snippet.thumbnails.default;
+  const { url, height, width } = video.snippet.thumbnails.medium;
   console.log(video);
   return (
     <div className="list__item">
-      <h2 onClick={() => onVideoSelect(video)}>{video.snippet.title}</h2>
       <div>
         <img src={url} alt="thumbnail" height={height} width={width} />
+        <h2 onClick={() => onVideoSelect(video)}>{video.snippet.title}</h2>
+        <p>{video.snippet.title}</p>
       </div>
     </div>
   );
